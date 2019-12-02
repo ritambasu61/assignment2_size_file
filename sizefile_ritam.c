58f5bbfb2f50b93ad3628d55c0122c59d4e6180e
ritambasu61 <57460456+ritambasu61@users.noreply.github.com>
2019-12-03 02:58:50 +0530

    Add files via upload
    determining the size of different type of inputs

---
+++ sizefile_ritam.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int a;float b;char c;double d;long int e;
+  printf("Size of int: %ld bytes\n",sizeof(a));
+  printf("Size of float: %ld bytes\n",sizeof(b));
+  printf("Size of char: %ld bytes\n",sizeof(c));
+  printf("Size of double: %ld bytes\n",sizeof(d));
+  printf("Size of long int: %ld bytes\n",sizeof(e));
+}
